d277af27e867fd0ecfc10267663723c2a3d6ff05
Nguyen Phan <nqphan212@gmail.com>
2015-08-10 15:11:09 -0400

    add fragments and modify them

---
+++ app/src/main/java/com/nguyenqphan/ribbit/SectionsPagerAdapter.java
@@ -0,0 +1,52 @@
+package com.nguyenqphan.ribbit;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.nguyenqphan.ribbit.MainActivity;
+import com.nguyenqphan.ribbit.R;
+
+import java.util.Locale;
+
+/**
+ * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    protected Context mContext;
+
+    public SectionsPagerAdapter(Context context,FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return new InboxFragment();
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        Locale l = Locale.getDefault();
+        switch (position) {
+            case 0:
+                return mContext.getString(R.string.title_section1).toUpperCase(l);
+            case 1:
+                return mContext.getString(R.string.title_section2).toUpperCase(l);
+                //return "Section Test".toUpperCase();
+
+        }
+        return null;
+    }
+}
